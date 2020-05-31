ce47ca42538574e8cefab83a32388995fee8905f
Ananya2306 <amitarora_1@yahoo.com>
2020-05-31 20:42:40 +0530

    index.html

--- sketch.js
+++ sketch.js
@@ -1,29 +1,55 @@
 var ball;
 
+var database,position;
+
 function setup(){
+    database = firebase.database();
+console.log(database);
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    
+    var ballposition = database.ref('ball/position');
+    ballposition.on("value",readposition,showError);
 }
 
 function draw(){
     background("white");
+    if(position !== undefined){
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
+
     drawSprites();
 }
-
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
 }
+
+function showError(){
+    console.log("error");
+    
+}
+
+function readposition(data){
+    position = data.val();
+    console.log(position.x);
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function writePosition(x,y){
+    database.ref('ball/position').set({ 
+        'x' : ball.x + x,
+        'y' : ball.y + y
+    })
+    
+}
+
